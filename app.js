10755ff82e484eaa382fdfbe6ef73dea052e2af7
Arnau <agomez1f@gmail.com>
2020-10-28 21:09:52 +0100

    I added a makeshift users database

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
-const path = require('path')
+const path = require('path');
 
-const bodyParser = require('body-parser')
-const express = require('express')
+const bodyParser = require('body-parser');
+const express = require('express');
 
-const users = [] // users database
+const users = []; // users database
 
-const app = express()
-app.set('view engine', 'pug')
-app.set('views', 'views')
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public'))); // serve CSS files as static files
-
 
 /******************
  * OUR APP ROUTES *
  ******************/
 
 app.get('/', (req, res, next) => {
-  res.render('index', {pageTitle: "Main site"})
-})
+  res.render('index', {pageTitle: 'Main site'});
+});
+
 app.get('/users', (req, res, next) => {
-  
-  next()
-})
-app.post('/add-user', (req, res, next)=>{
-  const {userName} = req.body
-  users.push({userName})
-  console.log(users)
-  res.redirect('/users')
-})
+  res.render('users', {pageTitle: 'Users list', users});
+});
+
+app.post('/add-user', (req, res, next) => {
+  const {userName} = req.body;
+  users.push({userName});
+  console.log(users);
+  res.redirect('/users');
+
+});
 
 /***************
  * 404 MESSAGE *
  ***************/
 
 app.use('/', (req, res, next) => {
-  res.status(404).render('404', {pageTitle: "404 Page not found. "})
-})
+  res.status(404).render('404', {pageTitle: '404 Page not found. '});
+});
 
 /********************
  * START THE SERVER *
